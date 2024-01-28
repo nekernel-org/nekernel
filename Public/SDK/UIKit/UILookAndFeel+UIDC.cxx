87913aa62de0edfcbde9d9fdb938968d1b511a5f
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-01-28 17:12:48 +0100

    Public: Refactor and cleaned up Public/ folder.
    - Many cleanups and new emerging APIs (GPUKit, SystemKit)
    
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- Public/SDK/UIKit/UILookAndFeel+UIDC.cxx
+++ Public/SDK/UIKit/UILookAndFeel+UIDC.cxx
@@ -1,2 +1,10 @@
-#include "UILookAndFeel+UIDC.hxx"
+/*
+ *	========================================================
+ *
+ *	hCore
+ * 	Copyright Mahrouss Logic, all rights reserved.
+ *
+ * 	========================================================
+ */
 
+#include "UILookAndFeel+UIDC.hxx"
