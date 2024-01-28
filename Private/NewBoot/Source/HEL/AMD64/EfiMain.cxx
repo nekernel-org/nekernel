4a21ce8c1d7e7f2e080d2c348c1ba01558062611
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-01-28 22:29:45 +0100

    Kernel: Got NewBoot to boot on EFI.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- Private/NewBoot/Source/HEL/AMD64/EfiMain.cxx
+++ Private/NewBoot/Source/HEL/AMD64/EfiMain.cxx
@@ -7,10 +7,11 @@
  *      ========================================================
  */
 
-#include "EFIApi.hxx"
+#include "Protocol.hxx"
 
 extern "C" int EfiMain(void *ImageHandle, EfiSystemTable *SystemTable)
 {
     SystemTable->conOut->output_string(SystemTable->conOut, L"HCORELDR: Preparing Handover structure...\r\n");
+
     return 0;
 }
--- Private/NewBoot/Source/HEL/AMD64/Protocol.hxx
+++ Private/NewBoot/Source/HEL/AMD64/Protocol.hxx
@@ -13,11 +13,18 @@
 
 namespace HEL
 {
+using namespace hCore;
+
 struct HandoverHeader final
 {
-    int magic;
-    int type;
-    int arch;
+    Int32 magic;
+    Int32 type;
+    Int32 arch;
     // WIP
 };
 } // namespace HEL
+
+#define kHandoverMagic 0xBAD55
+
+#define kBaseHandoverStruct 0x10000000
+#define kHandoverStructSz sizeof(HEL::HandoverHeader)
