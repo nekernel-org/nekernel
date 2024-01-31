0269e7f53f44d8e62ec35280d4adde7aa3ef1c6c
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-01-31 14:45:10 +0100

    Fix: stupid CxxKitRT
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- Private/Source/CxxKitRT.cxx
+++ Private/Source/CxxKitRT.cxx
@@ -15,29 +15,6 @@ using namespace HCore;
 
 // unimplemented _HintTell
 void _HintTell(_HintMessage* ppMsg, _HintId* pId) {
-  switch (*pId) {
-    case kErrorExecutable: {
-      const char* msg = "CodeManager doesn't recognize this executable.";
-      rt_copy_memory((const voidPtr)msg, *ppMsg, string_length(msg));
-      break;
-    }
-    case kErrorExecutableLib: {
-      const char* msg = "CodeManager doesn't recognize this library.";
-      rt_copy_memory((const voidPtr)msg, *ppMsg, string_length(msg));
-      break;
-    }
-    case kErrorFileNotFound: {
-      const char* msg = "FileManager doesn't find this file.";
-      rt_copy_memory((const voidPtr)msg, *ppMsg, string_length(msg));
-      break;
-    }
-    case kErrorNoNetwork: {
-      const char* msg =
-          "NetworkManager doesn't detect any WiFi/Ethernet connection.";
-      rt_copy_memory((const voidPtr)msg, *ppMsg, string_length(msg));
-      break;
-    }
-    default:
-      break;
-  }
+  (void)ppMsg;
+  (void)pId;
 }
