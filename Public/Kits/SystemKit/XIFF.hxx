a0c3424672b698332be48a790d44cd71c4e9b043
Amlal El Mahrouss <amlalelmahrouss@icloud.com>
2024-02-21 10:01:51 +0100

    XIFF: Update container header.
    Signed-off-by: Amlal El Mahrouss <amlalelmahrouss@icloud.com>

--- Public/Kits/SystemKit/XIFF.hxx
+++ Public/Kits/SystemKit/XIFF.hxx
@@ -11,22 +11,18 @@
 
 ------------------------------------------------------- */
 
-struct XiffAudioHeader {
-  unsigned char mag[4];               // XAUD string
-  unsigned int overall_size;          // overall size of file in bytes
-  unsigned char specific_mag[4];      // WAVE string
-  unsigned char fmt_chunk_marker[4];  // fmt string with trailing null char
-  unsigned int length_of_fmt;         // length of the format data
-  unsigned int format_type;  // format type. 1-PCM, 3- IEEE float, 6 - 8bit A
-                             // law, 7 - 8bit mu law
-  unsigned int channels;     // no.of channels
-  unsigned int sample_rate;  // sampling rate (blocks per second)
-  unsigned int byterate;     // SampleRate * NumChannels * BitsPerSample/8
-  unsigned int block_align;  // NumChannels * BitsPerSample/8
-  unsigned int bits_per_sample;  // bits per sample, 8- 8bits, 16- 16 bits etc
-  unsigned char data_chunk_header[4];  // DATA string or FLLR string
-  unsigned int data_size;  // NumSamples * NumChannels * BitsPerSample/8 - size
-                           // of the next chunk that will be read
+/***
+ * @brief Generic XIFF header
+ * Used by XIFF based containers.
+ */
+
+struct XiffHeader {
+  unsigned char mag[5];      // XIFF string (includes \0)
+  unsigned int size;         // overall size of header (XiffHeader) in bytes
+  unsigned int format_type;  // format type. generic
+  unsigned char specific_mag[4];      // The sub header magic
+  unsigned int specific_size;         // length of the format data
+  unsigned int specific_format_type;  // format type. generic
 };
 
 #define kXIFFVideo "XVFF"
