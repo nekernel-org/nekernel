a90ef5f5d9b047da087cf925ae6d5e6de3b18b32
Amlal El Mahrouss <amlalelmahrouss@icloud.com>
2024-03-05 17:32:36 +0000

    Fix powershell script.

--- Private/NewBoot/Source/download-edk.ps1
+++ Private/NewBoot/Source/download-edk.ps1
@@ -1,2 +1,4 @@
 $client = new-object System.Net.WebClient
-$client.DownloadFile("https://retrage.github.io/edk2-nightly/bin/DEBUGX64_OVMF.fd","C:\micro-kernel-Amllx-trunk-patch-989e\Private\NewBoot\Source\OVMF.fd")  
+$output = "$PSScriptRoot\OVMF.fd"
+
+$client.DownloadFile("https://retrage.github.io/edk2-nightly/bin/DEBUGX64_OVMF.fd", $output)  
