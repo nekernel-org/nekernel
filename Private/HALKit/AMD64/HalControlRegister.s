159d312c0dd3c02995950270e173cc10ade273cf
Amlal El Mahrouss <amlal@el-mahrouss-logic.com>
2024-03-14 16:13:09 +0100

    HalControlRegister.s: Add Copyright header.

--- Private/HALKit/AMD64/HalControlRegister.s
+++ Private/HALKit/AMD64/HalControlRegister.s
@@ -1,3 +1,9 @@
+/* -------------------------------------------
+
+    Copyright Mahrouss Logic
+
+------------------------------------------- */
+
 .globl write_cr3
 .globl write_cr0
 .globl read_cr2
@@ -6,30 +12,31 @@
 .globl flush_tlb
 
 .section .text
-    flush_tlb:
-        mov %rsi, %cr3
-        mov %cr3, %rsi
-        xor %rax, %rax
-        ret
 
-    read_cr3:
-        movq %rax, %cr3
-        ret
+flush_tlb:
+    mov %rsi, %cr3
+    mov %cr3, %rsi
+    xor %rax, %rax
+    ret
 
-    read_cr0:
-        movq %rax, %cr0
-        ret
+read_cr3:
+    movq %rax, %cr3
+    ret
 
-    read_cr2:
-        movq %rax, %cr2
-        ret
+read_cr0:
+    movq %rax, %cr0
+    ret
 
-    write_cr3:
-        movq %cr3, %rdi
-        ret
+read_cr2:
+    movq %rax, %cr2
+    ret
 
-    write_cr0:
-        movq %cr0, %rdi
-        ret
+write_cr3:
+    movq %cr3, %rdi
+    ret
+
+write_cr0:
+    movq %cr0, %rdi
+    ret
 
 
