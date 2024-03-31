48ed22614592c30bd1dc1f98f3988d05454e6c3b
Amlal El Mahrouss <amlal@el-mahrouss-logic.com>
2024-03-31 10:39:19 +0200

    SystemLib: Update DlgMsgBox call.
    - Return an exit code.
    - Mark title and format const.
    
    Signed-off-by: Amlal El Mahrouss <amlal@el-mahrouss-logic.com>

--- Public/Developer/SystemLib/Headers/Dialog.h
+++ Public/Developer/SystemLib/Headers/Dialog.h
@@ -8,9 +8,10 @@
 
 #include <Headers/Window.h>
 
-/// @brief Shows an message box with a formatting.
+/// @brief Shows an message box according to format.
 /// @param title the message box title
 /// @param format the format
 /// @param va_list the va args, that goes along with it.
-/// @return void this function returns nothing.
-CA_EXTERN_C VoidType DlgMsgBox(CharacterTypeUTF8* title, CharacterTypeUTF8* format, ...);
+/// @return 0: the user clicked Ok
+/// @return > 0: User clicked on specific button.
+CA_EXTERN_C Int32Type DlgMsgBox(const CharacterTypeUTF8* title, const CharacterTypeUTF8* format, ...);
