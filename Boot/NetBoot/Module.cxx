6cf21523d05fb1182ac793925255efe2de68263e
Amlal EL Mahrouss <amlal@softwarelabs.com>
2024-06-12 18:36:30 +0200

    MHR-?: OTA updates?
    Signed-off-by: Amlal EL Mahrouss <amlal@softwarelabs.com>

--- Boot/NetBoot/Module.cxx
+++ Boot/NetBoot/Module.cxx
@@ -12,7 +12,7 @@
 
 EXTERN_C Int32 ModuleMain(Void)
 {
-	/// - Find a network drive called "/Remote/NewOSKrnl"
+	/// - Find a network drive called "/Remote/newoskrnl"
 	/// - Download our image
 	/// - Boot from it.
 
--- Boot/NetBoot/NetBoot.hxx
+++ Boot/NetBoot/NetBoot.hxx
@@ -10,3 +10,18 @@
 #pragma once
 
 #include <NewKit/Defines.hpp>
+
+/// @brief the internet header is used to download updates OTA.
+typedef struct NetBootInternetHeader
+{
+	CharacterTypeUTF8 NB1; /// magic char 1 'N'
+	CharacterTypeUTF8 NB2; /// magic char 2 'E'
+	CharacterTypeUTF8 NB3; /// magic char 3 'T'
+	CharacterTypeUTF8 NB4; /// magic char 4 'B'
+
+	CharacterTypeUTF8 PatchName[255]; /// example: ColdChoco
+	NewOS::Int32 PatchLength; /// the patch length.
+	CharacterTypeUTF8 PatchTarget[255]; /// the target file.
+	NewOS::Boolean ImpliesROM; /// does it implies EEPROM patching?
+} NetBootInternetHeader;
+
