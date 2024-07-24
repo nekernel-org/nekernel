43981bf8ecacb5f95070fa4ff502941d9e3251b4
Amlal <amlal@zka.com>
2024-07-24 20:48:25 +0200

    [MHR-36]: Removed HAL directory, replaced with Samples directory.
    Signed-off-by: Amlal <amlal@zka.com>

--- Samples/SampleHAL/x86_64.mk
+++ Samples/SampleHAL/x86_64.mk
@@ -16,20 +16,20 @@ LD_FLAGS=-e __at_enter --subsystem=17
 
 OBJ=*.o
 
+
 REM=rm
 REM_FLAG=-f
 
 FLAG_ASM=-f win64
-FLAG_GNU=-fshort-wchar -D__DDK_AMD64__ -mgeneral-regs-only -mno-red-zone -D__KERNEL__ \
-	-DEFI_FUNCTION_WRAPPER -I../ -I../../ -I./ -c -ffreestanding  -std=c17 -D__HAVE_MAHROUSS_APIS__ -D__MAHROUSS__ -D__BOOTLOADER__ -I./
+FLAG_GNU=-fshort-wchar -D__EFI_x86_64__ -mgeneral-regs-only -mno-red-zone -D__KERNEL__ -DEFI_FUNCTION_WRAPPER -I../ -I../../ -I./ -c -ffreestanding  -std=c17 -D__HAVE_MAHROUSS_APIS__ -D__MAHROUSS__ -D__BOOTLOADER__ -I./
 
 .PHONY: invalid-recipe
 invalid-recipe:
-    @echo "invalid-recipe: Use make all instead."
+	@echo "invalid-recipe: Use make all instead."
 
 .PHONY: all
 all: compile-amd64
-	$(LD_GNU) $(OBJ) $(LD_FLAGS) -o Hello.exe
+	$(LD_GNU) $(OBJ) $(LD_FLAGS) -o SampleHAL.exe
 
 ifneq ($(DEBUG_SUPPORT), )
 DEBUG =  -D__DEBUG__
@@ -38,11 +38,11 @@ endif
 .PHONY: compile-amd64
 compile-amd64:
 	$(WINDRES) DriverRsrc.rsrc -O coff -o DriverRsrc.o
-	$(CC_GNU) $(FLAG_GNU) $(DEBUG) $(wildcard *.c) $(wildcard ../../DDK/*.cc) $(wildcard ../../DDK/*.c) $(wildcard ../../DDK/*.S)
+	$(CC_GNU) $(FLAG_GNU) $(DEBUG) $(wildcard *.c) $(wildcard ../../DDK/*.c) $(wildcard ../../DDK/*.S)
 
 .PHONY: clean
 clean:
-	$(REM) $(REM_FLAG) $(OBJ) Hello.exe
+	$(REM) $(REM_FLAG) $(OBJ) SampleHAL.exe
 
 .PHONY: help
 help:
