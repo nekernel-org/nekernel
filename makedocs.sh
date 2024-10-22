8c05685d43ce29a3ad6b0f20293ee98106531ed3
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-10-22 05:51:42 +0200

    IMP: Add makedocs file.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

---
+++ makedocs.sh
@@ -0,0 +1,5 @@
+# !/bin/zsh
+
+mkdir -p html
+
+XSDocgen --source ./dev --output ./html --project-name "ZKA" --project-copyright "ZKA Web Services Co &copy; %Y - All rights Reserved" --project-version "930.2024.1" --company-name "ZKA Web Services Co"
