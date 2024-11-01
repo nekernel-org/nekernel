be27da08ba435370691870d6e52d650fb44714e0
Amlal El Mahrouss <113760121+Amlal-ElMahrouss@users.noreply.github.com>
2024-11-01 17:55:17 +0100

    Update HPFS.h
    Update specs.

--- dev/ZKAKit/FSKit/HPFS.h
+++ dev/ZKAKit/FSKit/HPFS.h
@@ -11,8 +11,8 @@
 /// @file HPFS.h
 /// @brief HPFS filesystem support.
 
-#define kHPFSVersion  0x01000
-#define kHPFSMagic	  "  HPFS"
+#define kHPFSVersion  0x0100
+#define kHPFSMagic    "  HPFS"
 #define kHPFSMagicLen 8
 
 #define kHPFSMinimumDiskSize (gib_cast(64))
@@ -20,14 +20,12 @@
 enum
 {
 	kHPFSInvalidDrive,
-	kHPFSCDROMDrive,
 	kHPFSHDDDrive,
 	kHPFSSSDDrive,
-	kHPFSUSBDrive,
+	kHPFSMassStorageDrive,
+	kHPFSSCSIDrive,
+	kHPFSDriveCount,
 };
 
 struct HPFS_EXPLICIT_BOOT_SECTOR;
 
-struct HPFS_EXPLICIT_BOOT_SECTOR final
-{
-};
