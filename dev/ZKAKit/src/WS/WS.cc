49d8bce2413cc9497214cca88584917d9e9088c1
Amlal EL Mahrouss <amlalelmahrouss@icloud.com>
2024-11-28 18:29:13 +0100

    IMP: Improve Kernel architecture.
    Signed-off-by: Amlal EL Mahrouss <amlalelmahrouss@icloud.com>

--- dev/ZKAKit/src/WS/WS.cc
+++
@@ -1,7 +0,0 @@
-/* -------------------------------------------
-
-	Copyright (C) 2024, ELMH Group, all rights reserved.
-
-------------------------------------------- */
-
-#include <Modules/FB/WS.h>
