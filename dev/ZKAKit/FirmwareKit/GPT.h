02ea678be07b25c0601ac2e89deb26196be768d2
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-12-08 08:57:34 +0100

    Add GPT.h header.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

---
+++ dev/ZKAKit/FirmwareKit/GPT.h
@@ -0,0 +1,9 @@
+/* -------------------------------------------
+
+	Copyright (C) 2024, ELMH GROUP, all rights reserved.
+
+------------------------------------------- */
+
+#pragma once
+
+#include <NewKit/Defines.h>
