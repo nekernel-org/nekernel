ac62a264fe08839b46160e8ce218a32012883cd7
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2025-03-07 12:10:48 +0100

    ADD: WIP: Filesystem format tool. (under EPM)

---
+++ public/tools/make_fs/make_fs.cc
@@ -0,0 +1,16 @@
+/* -------------------------------------------
+
+	Copyright (C) 2025, Amlal EL Mahrouss, all rights reserved.
+
+	FILE: NeFS.h
+	PURPOSE: NeFS (New extended File System) support.
+
+   ------------------------------------------- */
+
+#include <FSKit/NeFS.h>
+
+int main(int argc, char** argv)
+{
+    
+    return 0;
+}
