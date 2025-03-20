d1b7d34c43ec463815c448e7677fc64ec936504a
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2025-03-20 15:06:01 +0100

    DiskImage.h: Completed documentation for DIFormatDiskToFile

--- public/frameworks/DiskImage.fwrk/headers/DiskImage.h
+++ public/frameworks/DiskImage.fwrk/headers/DiskImage.h
@@ -14,6 +14,13 @@
 #include <uuid/uuid.h>
 #include <LibSCI/SCI.h>
 
+/// @brief EPM format disk to **out_name** 
+/// @param disk_name The partition name
+/// @param sector_sz The sector size
+/// @param block_cnt The block count
+/// @param disk_sz The disk size (in bytes)
+/// @param out_name the output filesystem path.
+/// @return Status code regarding completion.
 SInt32 DIFormatDiskToFile(const char* disk_name = "Disk",
 						  int		  sector_sz = 512,
 						  const int	  block_cnt = 1,
