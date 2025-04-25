f11b6571d6d45b6d283761031b64b205fed63cf1
Amlal <amlal@nekernel.org>
2025-04-25 13:10:03 +0200

    dev: compiler error fixes.
    Signed-off-by: Amlal <amlal@nekernel.org>

--- dev/kernel/HALKit/AMD64/HalDebugPort.cc
+++ dev/kernel/HALKit/AMD64/HalDebugPort.cc
@@ -14,24 +14,6 @@
 
 namespace Kernel {
 void rt_debug_listen(DebuggerPortHeader* theHook) noexcept {
-  if (theHook == nullptr) return;
-
-  for (UInt32 i = 0U; i < kDebugMaxPorts; ++i) {
-    HAL::rt_out16(theHook->fPort[i], kDebugMag0);
-    HAL::rt_wait_400ns();
-
-    HAL::rt_out16(theHook->fPort[i], kDebugMag1);
-    HAL::rt_wait_400ns();
-
-    HAL::rt_out16(theHook->fPort[i], kDebugMag2);
-    HAL::rt_wait_400ns();
-
-    HAL::rt_out16(theHook->fPort[i], kDebugMag3);
-    HAL::rt_wait_400ns();
-
-    if (HAL::rt_in16(theHook->fPort[i]) != kDebugUnboundPort) ++theHook->fPortCnt;
-
-    HAL::rt_wait_400ns();
-  }
+  NE_UNUSED(theHook);
 }
 }  // namespace Kernel
