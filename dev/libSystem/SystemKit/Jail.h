d546e35937521c868150a0807f30a2e2b1f69bb8
Amlal El Mahrouss <amlal@nekernel.org>
2025-07-29 15:17:36 +0100

    feat: libSystem: implement 'Jail.h' header.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- dev/libSystem/SystemKit/Jail.h
+++ dev/libSystem/SystemKit/Jail.h
@@ -9,7 +9,7 @@
 #include <libSystem/SystemKit/System.h>
 
 /// @file Jail.h
-/// @brief NeKernel Jail System
+/// @brief NeKernel Jail System, part of OpenEnclave.
 
 struct JAIL_INFO;
 struct JAIL;
@@ -20,3 +20,16 @@ struct JAIL_INFO {
   SInt32 fJailHash;
   SInt64 fACL;
 };
+
+/// @brief Jail information (we grab a JAIL from JailGetCurrent())
+struct JAIL {
+  struct JAIL_INFO* fServer;
+  struct JAIL_INFO* fClient;
+  SInt32            fJailHash;
+  SInt32            fParentID;
+  SInt64            fACL;
+};
+
+/// @brief Get the current jail
+/// @return Pointer to the current jail structure, or NULL if not in a jail
+IMPORT_C struct JAIL* JailGetCurrent(Void);
