55d4ff5b5d8ec106d9f42e27c5218911d7c47d83
Amlal El Mahrouss <amlal@nekernel.org>
2025-08-01 07:55:41 +0100

    feat: tools/mgmt.oe: tiny source tweaks.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- public/tools/mgmt.oe/src/CommandLine.cc
+++ public/tools/mgmt.oe/src/CommandLine.cc
@@ -16,11 +16,11 @@ SInt32 _NeMain(SInt32 argc, Char* argv[]) {
   LIBSYS_UNUSED(argc);
   LIBSYS_UNUSED(argv);
 
-  kJailSrv = JailGetCurrent();
+  kJailSrv = ::JailGetCurrent();
 
   MUST_PASS(kJailSrv);
 
-  PrintOut(nullptr, "%s", "mgmt.oe - OpenEnclave Management Tool.");
+  ::PrintOut(nullptr, "%s", "mgmt.oe: OpenEnclave Management Tool.");
 
   /// @note JailGetCurrent returns client as nullptr if we're not that client (we'll not be able to access the jail then)
   if (kJailSrv->fClient == nullptr) return EXIT_FAILURE;
