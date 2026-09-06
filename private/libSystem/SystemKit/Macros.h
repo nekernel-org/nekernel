a3dde429887ee63b801eadea50a8555a57db6267
Amlal El Mahrouss <amlal@nekernel.org>
2026-09-06 03:18:29 +0200

    CHORE: src: Add Thread API and better ifdefs
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- private/libSystem/SystemKit/Macros.h
+++ private/libSystem/SystemKit/Macros.h
@@ -17,8 +17,13 @@
 #define ATTRIBUTE(X) __attribute__((X))
 #endif
 
+#ifndef __THREAD_SAFE
 #define __THREAD_SAFE ATTRIBUTE(thread_safe)
+#endif
+
+#ifndef __THREAD_UNSAFE
 #define __THREAD_UNSAFE ATTRIBUTE(thread_unsafe)
+#endif
 
 #ifdef __cplusplus
 #define IMPORT_CXX extern "C++"
